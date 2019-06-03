17e193897d38392f1f686c9ee467ce09b8387cbf
TSSahil <35215029+TSSahil@users.noreply.github.com>
2019-06-03 16:09:20 +0530

    use of break keyword
    this shows the use of break keyword in c language i.e it will exit the loop as soon as it encounters break

---
+++ break.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int i;
+    int n;
+    int sum;
+	for(i=0;i<15;i++)
+	{
+	    scanf("%d",&n);
+	    if(n<0)
+	    break;
+	    n=8;
+	    
+	}
+	printf("sum is %d\t",n);
+	return 0;
+}
